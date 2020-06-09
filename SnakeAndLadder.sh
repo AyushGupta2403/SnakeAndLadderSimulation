a3eca2ff59ecd27492e6c0afe2749b92f2d898e7
Ayush <ayushmanojgupta24@gmail.com>
2020-06-09 22:14:15 +0530

    Added Random To Get Value From 1 to 6

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -3,3 +3,5 @@ echo "------------Welcome to Snake And Ladder Simulation-------------"
 
 #declaring position at start to 0
 position=0
+#generating random number between 1 to 6
+number=$((1+RANDOM%6))
