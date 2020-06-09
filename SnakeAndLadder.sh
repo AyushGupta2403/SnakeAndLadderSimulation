df98298ca2af9b88f173e6f4125df5abe8dab4d8
Ayush <ayushmanojgupta24@gmail.com>
2020-06-09 22:37:50 +0530

    Added Player Have To Reach Exact on 100 Position

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,10 +1,52 @@
 #!/bin/bash -x
-#declaring function for snakeLadderGame
+#declaring position at start to 0
+
+  function ladderfunction()
+  {
+ 	position=$(($position+$die))
+   if (($position > 100 ))
+   then
+   position=$currentPosition
+   fi
+	echo " Current Position after climbing ladder for Player : $msg  ->" $position
+	echo ""
+  }
+
+
+  function snakefunction()
+  {
+
+   position=$(($position-$die))
+   if(($position < 0))
+   then
+    position=0
+   fi
+   if(($position < 100 ))
+   then
+    currentPosition=$position
+   fi
+ 		echo " Current Position after snake bite for Player : $msg ->" $position
+		echo ""
+   }
+
+
+
+   function noPlayfunction()
+   {
+    position=$position
+    echo " Current Position after no Play for Player : $msg ->" $position
+	 echo "" 
+	}
+
 function snakeLadderGame()
 {
+msg=$1
+echo "This is Player $msg"
 position=0
 count=0
-while(( $position < 100 ))
+position=$currentPosition
+
+while [[ $position -ne 100 ]]
 do
 die=$((1+RANDOM%6))
 option=$((RANDOM%3))
@@ -14,31 +56,20 @@ snake=2
 	case $option in
 #noPlay option:The player does not move any position
 			$noPlay)
-				position=$position
-
-				echo " Current Position after no Play for Player : $msg ->" $position
+						 noPlayfunction
 						;;
 #ladder option:The player moves die number of position ahead in the game
 			$ladder)
 
-				position=$(($position+$die))
-
-				echo " Current Position after climbing ladder for Player : $msg ->" $position
+						 ladderfunction
 						;;
 #snake option:The player moves die number of positions behind in the game
 			$snake)
-
-				position=$(($position-$die))
-
-				if(($position < 0))
-             then
-                position=0
-             fi
-
-				echo " Current Position after snake bite for Player : $msg ->" $position
+						 snakefunction
 						;;
 	esac
 done
+
 }
 
- snakeLadderGame
+snakeLadderGame
